5753b6205e58a8f9745ade0efdddeefde60267cc
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-11-20 13:10:11 -0300

    Escrevendo arquivo e rescrevendo se adicionar no final do arquivo.

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] alunos = new String[]{
+                "Francyne",
+                "Julia",
+                "Bryan",
+                "Camila",
+                "Renata",
+                "Samuel",
+                "Gabriel",
+                "Matheus",
+                "Arthur",
+                "Fernanda",
+                "Emilly",
+                "Miguel Henrique",
+                "Verônica",
+                "Graciella",
+                "Maria Clara",
+                "Sthela",
+                "Henrique",
+                "Milena",
+                "Victor",
+                "Eduarda",
+                "Ana Clara",
+                "Gabriel",
+                "Ana Luisa",
+                "Pedro",
+                "Kemily",
+                "Guilherme",
+                "Miguel Serra",
+                "Guilherme Silva",
+                "Pedro Sampaio",
+                "Nathan",
+                "Rafaela",
+                "Carlos Daniel",
+                "Rita",
+                "Renan",
+                "Diego",
+                "Isabela",
+                "Murilo",
+                "Caique",
+                "Maiara",
+                "Nathália",
+                "Nicolas",
+                "Kamily",
+                "Wendel"
+        };
+
+        String path = "C:\\Users\\ljele\\IdeaProjects\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+            for (String linha : alunos) {
+                bw.write(linha + ", ");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
